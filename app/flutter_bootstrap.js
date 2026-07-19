6c3c0f3eaa489c0429a23ecb7be9948baa87c3d9
Caleb <eckelbec1@gmail.com>
2026-07-19 14:58:06 -0500

    app: v479 keyless web build

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "326463961" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2434145995" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
