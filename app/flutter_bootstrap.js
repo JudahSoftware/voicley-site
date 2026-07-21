1c89ab343cc90297fbd4bf95132ad68d22853c0b
Caleb <eckelbec1@gmail.com>
2026-07-20 19:24:23 -0500

    app v522

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1149016934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2784264582" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
