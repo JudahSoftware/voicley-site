5141fa57e9790ccedecd89d2cee5d520b722dfd5
Caleb <eckelbec1@gmail.com>
2026-07-21 20:10:38 -0500

    app: Voicley web v551 (four-corner box layout + chevron-only fold)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3647968890" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "812831103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
