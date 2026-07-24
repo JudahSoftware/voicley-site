7d1a76cd79dac780dd1a3ba4b36b62f28592ed9d
Caleb <eckelbec1@gmail.com>
2026-07-24 09:15:41 -0500

    app: v601 - drag subfolders to move them

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3731232996" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3058278329" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
