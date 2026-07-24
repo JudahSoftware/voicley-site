14b10955114b7ce6b07a85fba2eb9e593feb5ecf
Caleb <eckelbec1@gmail.com>
2026-07-24 13:06:30 -0500

    Deploy web app v607 (auto-record off; single Move-to-Folder)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "724281192" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "376295868" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
