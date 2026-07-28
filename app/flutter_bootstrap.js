b953d99bb37e3eef41ee195b3f428f05f42c53a5
Caleb <eckelbec1@gmail.com>
2026-07-27 22:54:44 -0500

    web: Voicley v771 (keyless public build)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3774551010" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2593841585" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
